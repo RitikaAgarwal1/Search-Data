c1b9e43795f2cf540a2a5ee2da126c359a3db8d4
Ritika Agarwal <Ritika.Agarwal1@gds.ey.com>
2022-02-12 18:17:41 +0530

    new feature

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 
+  // hiding input box for phone number
+  $("form div input").eq(1).hide();
+
+  // Switching between phone number and email address
+  $(".switch-container span").on("click", function (event) {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+    event.target.dataset.value == 'email' ? $("form div input").eq(0).show() : $("form div input").eq(0).hide();
+    event.target.dataset.value == 'phone' ? $("form div input").eq(1).show() : $("form div input").eq(1).hide();
+  });
+
   // Triggers user detail method when clicking on Go button
   $("#btn-search").on("click", async function (e) {
     userDetail(e);
@@ -20,16 +31,24 @@ $(document).ready(function () {
   async function userDetail(e) {
     e.preventDefault();
     localStorage.clear(); //Clears storage for next request
-    value = $('input[type="text"]').val().toLowerCase();
+
+    // assiging index of the input box
+    let index;
+    $('input').each(function (i, obj) {
+      if (obj.style.cssText != "display: none;"){
+        index = i;
+      }
+    });
+    value = $('input').val().toLowerCase();
 
     let email, phone;
     value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/) ? email = true : email = false;
-    value.match(/\d{10,}/)? phone = true: phone = false;
+    value.match(/\d{10,}/) ? phone = true : phone = false;
 
     if (email || (phone && value.length == 10)) {
       document.querySelector('input[type="text"]').parentNode.classList.remove("error");
       try {
-        const url = email? 'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + value: 'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + value;
+        const url = email ? 'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + value : 'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + value;
         const response = await fetch(url);
         const contents = await response.text();
         console.log(contents);
@@ -39,6 +58,8 @@ $(document).ready(function () {
         console.log(error);
       }
     } else if (!email && (!phone || value.length !== 10)) {
+
+      index == 0? document.querySelector('input[type="text"]').parentNode.querySelector('.error-msg').innerHTML = 'Please enter a valid email address': document.querySelector('input[type="text"]').parentNode.querySelector('.error-msg').innerHTML = 'Please enter a valid phone number';
       document.querySelector('input[type="text"]').parentNode.classList.add("error");
     }
   }
